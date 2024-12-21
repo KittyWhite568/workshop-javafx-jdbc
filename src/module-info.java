ff6e9d55f776fd7166367bf5dc51cc7ef6313104
KittyWhite568 <kittywhite568@gmail.com>
2024-12-21 14:28:37 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,13 @@
 module workshop.javafx.jdbc {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql; 
     
-    opens application to javafx.graphics, javafx.fxml;
+    opens application to javafx.graphics, javafx.fxml, javafx.base;
     opens gui to javafx.fxml;
-   
+    opens model.services to javafx.base; 
+    opens model.entities to javafx.base;
+
+    exports model.entities;
 }
+
